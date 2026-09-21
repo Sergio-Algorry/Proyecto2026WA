c7c1d525b608e0fc21976e250cdcdc25c20a67dd
Sergio Algorry <sergioalgorry@hotmail.com>
2026-09-21 11:46:54 -0300

    HttpRespuesta

---
+++ Proyecto2026WA.Servicio/ServicioHTTP/HttpResp.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto2026WA.Servicio.ServicioHTTP
+{
+    public class HttpResp<T>
+    {
+        public bool Error { get; set; }
+        public T? Respuesta { get; set; }
+        public HttpResponseMessage? Response { get; set; }
+        public string? Mensaje 
+        { 
+            get => ObtenerError(); 
+            //set; 
+        }
+
+        public string ObtenerError()
+        {
+            if (Response == null)
+            {
+                return "No se obtuvo respuesta del servidor.";
+            }
+            else if (Response.IsSuccessStatusCode)
+            {
+                return string.Empty; 
+            }
+            else
+            {
+                string Mensaje = "";
+                var statusCode = Response.StatusCode;
+
+                #region Mensaje segun StatusCode
+                switch (statusCode)
+                {
+                    case System.Net.HttpStatusCode.Continue:
+                        break;
+                    case System.Net.HttpStatusCode.SwitchingProtocols:
+                        break;
+                    case System.Net.HttpStatusCode.Processing:
+                        break;
+                    case System.Net.HttpStatusCode.EarlyHints:
+                        break;
+                    case System.Net.HttpStatusCode.OK:
+                        Mensaje = "Solicitud exitosa.";
+                        break;
+                    case System.Net.HttpStatusCode.Created:
+                        break;
+                    case System.Net.HttpStatusCode.Accepted:
+                        break;
+                    case System.Net.HttpStatusCode.NonAuthoritativeInformation:
+                        break;
+                    case System.Net.HttpStatusCode.NoContent:
+                        break;
+                    case System.Net.HttpStatusCode.ResetContent:
+                        break;
+                    case System.Net.HttpStatusCode.PartialContent:
+                        break;
+                    case System.Net.HttpStatusCode.MultiStatus:
+                        break;
+                    case System.Net.HttpStatusCode.AlreadyReported:
+                        break;
+                    case System.Net.HttpStatusCode.IMUsed:
+                        break;
+                    case System.Net.HttpStatusCode.Ambiguous:
+                        break;
+                    case System.Net.HttpStatusCode.Moved:
+                        break;
+                    case System.Net.HttpStatusCode.Found:
+                        break;
+                    case System.Net.HttpStatusCode.RedirectMethod:
+                        break;
+                    case System.Net.HttpStatusCode.NotModified:
+                        break;
+                    case System.Net.HttpStatusCode.UseProxy:
+                        break;
+                    case System.Net.HttpStatusCode.Unused:
+                        break;
+                    case System.Net.HttpStatusCode.RedirectKeepVerb:
+                        break;
+                    case System.Net.HttpStatusCode.PermanentRedirect:
+                        Mensaje = "Redirección permanente.";
+                        break;
+                    case System.Net.HttpStatusCode.BadRequest:
+                        Mensaje = "Solicitud incorrecta - Intente nuevamente.";
+                        break;
+                    case System.Net.HttpStatusCode.Unauthorized:
+                        Mensaje = "No autorizado.";
+                        break;
+                    case System.Net.HttpStatusCode.PaymentRequired:
+                        break;
+                    case System.Net.HttpStatusCode.Forbidden:
+                        Mensaje = "Acceso denegado.";
+                        break;
+                    case System.Net.HttpStatusCode.NotFound:
+                        Mensaje = "Recurso no encontrado.";
+                        break;
+                    case System.Net.HttpStatusCode.MethodNotAllowed:
+                        break;
+                    case System.Net.HttpStatusCode.NotAcceptable:
+                        break;
+                    case System.Net.HttpStatusCode.ProxyAuthenticationRequired:
+                        break;
+                    case System.Net.HttpStatusCode.RequestTimeout:
+                        break;
+                    case System.Net.HttpStatusCode.Conflict:
+                        break;
+                    case System.Net.HttpStatusCode.Gone:
+                        break;
+                    case System.Net.HttpStatusCode.LengthRequired:
+                        break;
+                    case System.Net.HttpStatusCode.PreconditionFailed:
+                        break;
+                    case System.Net.HttpStatusCode.RequestEntityTooLarge:
+                        break;
+                    case System.Net.HttpStatusCode.RequestUriTooLong:
+                        break;
+                    case System.Net.HttpStatusCode.UnsupportedMediaType:
+                        break;
+                    case System.Net.HttpStatusCode.RequestedRangeNotSatisfiable:
+                        break;
+                    case System.Net.HttpStatusCode.ExpectationFailed:
+                        break;
+                    case System.Net.HttpStatusCode.MisdirectedRequest:
+                        break;
+                    case System.Net.HttpStatusCode.UnprocessableEntity:
+                        break;
+                    case System.Net.HttpStatusCode.Locked:
+                        break;
+                    case System.Net.HttpStatusCode.FailedDependency:
+                        break;
+                    case System.Net.HttpStatusCode.UpgradeRequired:
+                        break;
+                    case System.Net.HttpStatusCode.PreconditionRequired:
+                        break;
+                    case System.Net.HttpStatusCode.TooManyRequests:
+                        break;
+                    case System.Net.HttpStatusCode.RequestHeaderFieldsTooLarge:
+                        break;
+                    case System.Net.HttpStatusCode.UnavailableForLegalReasons:
+                        break;
+                    case System.Net.HttpStatusCode.InternalServerError:
+                        break;
+                    case System.Net.HttpStatusCode.NotImplemented:
+                        break;
+                    case System.Net.HttpStatusCode.BadGateway:
+                        break;
+                    case System.Net.HttpStatusCode.ServiceUnavailable:
+                        break;
+                    case System.Net.HttpStatusCode.GatewayTimeout:
+                        break;
+                    case System.Net.HttpStatusCode.HttpVersionNotSupported:
+                        break;
+                    case System.Net.HttpStatusCode.VariantAlsoNegotiates:
+                        break;
+                    case System.Net.HttpStatusCode.InsufficientStorage:
+                        break;
+                    case System.Net.HttpStatusCode.LoopDetected:
+                        break;
+                    case System.Net.HttpStatusCode.NotExtended:
+                        break;
+                    case System.Net.HttpStatusCode.NetworkAuthenticationRequired:
+                        break;
+                    default:
+                        break;
+                }
+                #endregion
+
+                return Mensaje;
+            }
+        }
+    }
+}
---
+++ Proyecto2026WA.Servicio/ServicioHTTP/HttpService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto2026WA.Servicio.ServicioHTTP
+{
+    public class HttpService
+    {
+    }
+}
